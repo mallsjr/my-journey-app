e9695142824045e01e7e86bef575792c1906a1d6
Michael Alls <allsjr@gmail.com>
2019-06-14 16:46:39 -0400

    add template, make components jumbo and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JumbotronComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
