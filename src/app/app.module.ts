f2e230c99d3ad6a036546445f4f66b6ce1a6ef30
Michael Alls <allsjr@gmail.com>
2019-06-15 23:31:06 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { JumbotronComponent } from "./jumbotron/jumbotron.component";
+import { FooterComponent } from "./footer/footer.component";
+import { CarouselComponent } from "./carousel/carousel.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { HomeComponent } from "./home/home.component";
+
+const appRoutes: Routes = [
+  { path: "gallery", component: GalleryComponent },
+  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     JumbotronComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent,
+    GalleryComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
